d7ae57d7520987d0095e58726a2b84cab39eb898
Andrzej Ośmiałowski <aosmialowski@icloud.com>
2020-12-09 22:54:42 +0100

    Clean macos/default.sh script

--- macos/defaults.sh
+++ macos/defaults.sh
@@ -1,18 +1,14 @@
 #!/bin/sh
 
-# Exit on any system other than macOS
 if [ "$(uname -s)" != "Darwin" ]; then
 	exit 0
 fi
 
-# Close any open System Preferences panes
 osascript -e 'tell application "System Preferences" to quit'
 
-# Ask for the administrator password upfront
 sudo -v
-
-# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 
 ###############################################################################
 # General UI/UX                                                               #
